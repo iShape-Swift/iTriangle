2f76fed016e6b7f8029abb937a41e4da34e9e424
Nail Sharipov <nailxsharipov@gmail.com>
2024-05-08 16:00:31 +0300

    up dependency

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/iShape-Swift/iFixFloat", .upToNextMajor(from: "1.7.0")),
         .package(url: "https://github.com/iShape-Swift/iShape", .upToNextMajor(from: "1.11.0")),
-        .package(url: "https://github.com/iShape-Swift/iOverlay", .upToNextMajor(from: "1.15.0"))
+        .package(url: "https://github.com/iShape-Swift/iOverlay", .upToNextMajor(from: "1.16.0"))
 //         .package(path: "../iFixFloat"),
 //         .package(path: "../iShape"),
 //         .package(path: "../iOverlay")
